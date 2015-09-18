aaafed66370dbaea1650ee253d46b495b15373e7
ryamada22 <ryamada@genome.med.kyoto-u.ac.jp>
2015-09-18 13:28:04 +0900

    3rd

--- R/foo.R
+++
@@ -1,9 +0,0 @@
-#' @export
-#' @useDynLib Rryamada
-
-foo <- function(x) {
-    stopifnot(is.numeric(x))
-
-    out <- .Fortran("foo", x = as.double(x), n = length(x), PACKAGE = "Rryamada")
-    return(out$x)
-}
--- R/mseedWrappers.R
+++
@@ -1,5 +0,0 @@
-# wrapper function to invoke test_hello_mseed
-test_hello_mseed <- function() {
-  result <- .Call("test_hello_mseed")
-  return(result)
-}
