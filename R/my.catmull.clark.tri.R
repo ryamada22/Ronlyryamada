9a0af6fb0a1221ead40dbb3b2273a5771f71c5ea
ryamada22 <ryamada@genome.med.kyoto-u.ac.jp>
2015-09-22 13:48:25 +0900

    catmull-clark

--- R/my.catmull.clark.tri.R
+++ R/my.catmull.clark.tri.R
@@ -1,9 +1,7 @@
 #' 3D triangle mesh smoothing
 #'
 #' These functions make triangle mesh smooth with Catmull-Clark subdivision
-#' @param x.vet is a list. The first element is 3-column matrix of coordinates o
-#' f vertices and the second element is triangular-mesh information object (outp
-#' ut of my.tri.vet() ).
+#' @param x.vet is a list. 
 #' @keywords shape triangulation mesh smoothing
 #' @export
 #' @examples
