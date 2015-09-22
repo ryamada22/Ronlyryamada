768f0d111ea5e9672ddda39daf275bce6257ad6f
ryamada22 <ryamada@genome.med.kyoto-u.ac.jp>
2015-09-22 17:45:42 +0900

    DEC conformal

--- R/my.DECconformal.R
+++ R/my.DECconformal.R
@@ -285,3 +285,64 @@ plot.sp.conformal <- function(xyz,faces.v,sp.tri,rho.f,col1=c(4,5)){
   #col3 <- gray((rho.f2+1)*0.5)
   shade3d(mesh.tri,col=col2)  
 }
+
+#' @export
+my.conformal.rho <- function(vertices,faces.v,rho.v,face=FALSE){
+  xyz.ori <- t(as.matrix(vertices)[2:4,])
+  n.v <- length(vertices) # 頂点数
+  n.f <- length(faces.v[1,]) # 三角形数
+  if(!face){
+    rho.f <- rho.fromVtoTri(rho.v,faces.v)
+  }else{
+    rho.f <- rho.v
+  }
+  
+  
+  # 三角形の面積
+  edge1 <- vertices[faces.v[2,]]-vertices[faces.v[1,]]
+	edge2 <- vertices[faces.v[3,]]-vertices[faces.v[1,]]
+	tmp <- edge1 * edge2
+  A <- Mod(Im(tmp))/2
+  
+  # rho の面積重みつき総和は0でないと「閉じ」ない
+  s <- sum(A*rho.f)
+  rho.f <- rho.f -s*A/sum(A)
+  
+  
+  E <- my.make.E.v(vertices,faces.v,rho.f)
+  E.re <- my.qMtorM(E)
+  
+  lambda.v <- my.inv.pow.2(E.re)[[1]]
+  
+  L <- my.make.L(vertices,faces.v)
+  L.q <- my.make.quatList(L)
+  L.re <- my.qMtorM(L.q)
+  
+  omega <- my.make.omega(vertices,faces.v,lambda.v)
+  
+  new.vertices <- as.quaternion(matrix(solve(L.re,omega),nrow=4))
+  xyz.new <- t(as.matrix(new.vertices)[2:4,])
+  mean.new <- apply(xyz.new,2,mean)
+  xyz.new. <- t(t(xyz.new)-mean.new)
+  max.new. <- max(abs(xyz.new.))
+  xyz.new.st <- xyz.new./max.new.
+  
+  new.q <- as.quaternion(t(cbind(rep(0,n.v),xyz.new.st)))
+  #ret <- xyz.new.st[,1]*Hi + xyz.new.st[,2]*Hj + xyz.new.st[,3]*Hk
+  
+  ret <- list(xyz.new=xyz.new.st,xyz.ori=sp.mesh$xyz,xyz.new.q=new.q,xyz.ori.q=vertices,faces.v=faces.v,E=E,L=L,lambda.v=lambda.v,omega=omega,n.psi=n.psi,rho.fx=rho.fx,rho.v=rho.v,rho.f=rho.f,sp.mesh=sp.mesh)
+  ret
+}
+
+#' @export
+my.sphereConformal <- function(n.psi,rho.fx){
+  sp.mesh <- my.sphere.tri.mesh(n.psi)
+  vertices.mat <- t(sp.mesh$xyz) # ３行行列化
+  vertices <- vertices.mat[1,]*Hi + vertices.mat[2,]*Hj + vertices.mat[3,]*Hk
+  edges <- sp.mesh$edge
+  faces.v <- t(sp.mesh$triangles)
+  rho.v <- rho.fx(sp.mesh$xyz) # 頂点における
+  out <- my.conformal.rho(vertices,faces.v,rho.v)
+  ret <- list(xyz.new=out$xyz.new,xyz.ori=sp.mesh$xyz,xyz.new.q=out$xyz.new.q,xyz.ori.q=vertices,faces.v=faces.v,E=out$E,L=out$L,lambda.v=out$lambda.v,omega=out$omega,n.psi=n.psi,rho.fx=rho.fx,rho.v=rho.v,rho.f=out$rho.f,sp.mesh=sp.mesh)
+  ret
+}
