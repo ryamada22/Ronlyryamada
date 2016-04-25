85f04f1a8c0b870fada1e7c95cde9e7d58bf4c43
ryamada22 <ryamada@genome.med.kyoto-u.ac.jp>
2016-04-26 08:55:35 +0900

    plot.flat.circle

--- R/my.plot.flat.circle.R
+++ R/my.plot.flat.circle.R
@@ -5,7 +5,7 @@
 #' @param R is a real value of radius
 #' @export
 #' @examples
-#' x <- matrix(rnorm(100*3),ncol=3)
+#' x <- matrix(rnorm(1000*3),ncol=3)
 #' x <- my.fit.sphere(x)
 
 my.fit.sphere <- function(x,R=1){
@@ -22,7 +22,7 @@ my.fit.sphere <- function(x,R=1){
 #' @export
 #' @examples
 #' require(RFOC)
-#' x <- matrix(rnorm(100*3),ncol=3)
+#' x <- matrix(rnorm(1000*3),ncol=3)
 #' x <- my.fit.sphere(x)
 #' x.sp <- TOSPHERE(x[,1],x[,2],x[,3])
 #' plot(x.sp[[1]],x.sp[[2]])
@@ -34,4 +34,28 @@ my.plot.flat.circle <- function(x){
 
 }
 
+#' Plot two hemi-spheres 
+#'
+#' plot two hemi-spheres
+#' @param x 2-column matrix with angles
+#' @export
+#' @examples
+#' require(RFOC)
+#' x <- matrix(rnorm(1000*3),ncol=3)
+#' x <- my.fit.sphere(x)
+#' x.sp <- TOSPHERE(x[,1],x[,2],x[,3])
+#' plot(x.sp[[1]],x.sp[[2]])
+#' x.sp.2 <- list(x.sp[[1]]/360*2*pi,x.sp[[2]]/360*2*pi)
+#' my.plot.hemispheres(x.sp.2)
 
+my.plot.hemispheres <- function(x){
+	n <- which(x[[2]] <= pi/2)
+	s <- which(x[[2]] > pi/2)
+	
+	N <- cbind(x[[2]][n] * cos(x[[1]][n]),x[[2]][n] * sin(x[[1]][n]))
+	s.r <- pi-x[[2]][s]
+	S <- cbind(s.r * cos(x[[1]][s]), s.r * sin(x[[1]][s]))
+	S[,2] <- S[,2] - pi
+	plot(rbind(N,S))
+
+}
